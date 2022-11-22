e71c6304bd407bd9806a3c345ac1325bf94a8e80
Lenka García <lenka.garcia1@gmail.com>
2022-11-22 12:58:50 +0100

    Druhá verze hry, ošetřeno uživatelské zadání

--- reverse_guessing_game.py
+++ reverse_guessing_game.py
@@ -20,13 +20,23 @@ computer_guess = random.randint(max(list_min) + 1, min(list_max) - 1)
 lives = 7
 print(f"\n\nTip počítače je {computer_guess}.")
 
+
 while lives != 0:
-    user_help = int(input(repeat_print))
+    while True:
+        user_help1 = input(repeat_print)
+        try:
+            user_help = int(user_help1)
+            if user_help < 1 or user_help > 3:
+                raise ValueError
+            break
+        except ValueError:
+            print("Zadaná hodnota musí být číslo 1, 2 nebo 3.")
     if user_help == 2:
         list_min.append(computer_guess)
     elif user_help == 3:
         list_max.append(computer_guess)
     elif user_help == 1:
+        result = "win"
         print("Počítač uhodl Vaše číslo a vyhrál")
         break
     computer_guess = random.randint(max(list_min) + 1, min(list_max) - 1)
@@ -34,7 +44,7 @@ while lives != 0:
     if lives == 0:
         print("Počítač vyčerpal všechny pokusy a číslo neuhodnul. Hra končí.")
         break
-    print(f"\nPočítač má ještě k dispozici {lives} pokusů.")
+    print(f"\nPočítač má k dispozici {lives} pokusů.")
     print(f"Nový tip počítače je číslo {computer_guess}")
 
         
